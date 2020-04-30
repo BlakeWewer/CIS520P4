1df26ed3c69ec14c50b487cd1970cb355ccd3419
BlakeWewer <bewewer@yahoo.com>
2020-04-30 14:09:55 -0500

    main - First Iteration

--- src/3way-mpi/diffCalc-MPI.c
+++ src/3way-mpi/diffCalc-MPI.c
@@ -25,28 +25,78 @@ void main(int argc, char* argv[])
     int i, rc, numTasks, rank, count, dest, source, tag = 1;
     MPI_STATUS Status;
 
-    MPI_INIT (&argc, &argv);
+    rc = MPI_INIT (&argc, &argv);
 
-    
+    if(rc != MPI_SUCCESS)
+    {
+        printf("\n\nError starting MPI.\n\n");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+    }
     MPI_Comm_size(MPI_COMM_WORLD, &numTasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if(rank == 0)
+    NUM_THREADS = numTasks;
+    printf("size = %d - rank = %d\n", numTasks, rank);
+    fflush(stdout);
+
+    gettimeofday(&t1, NULL);
+	if(rank == 0 ) 
     {
-        dest = 1;
-        source = 1;
-        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-        rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-    }
-    else if (rank == 1)
+		read_file();
+	}
+	gettimeofday(&t2, NULL);
+
+    MPI_Bcast(entries, NUM_ENTRIES * LINE_LENGTH, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    gettimeofday(&t3, NULL);
+
+    int line1_counter = -1;
+    int line2_counter = 0;
+    char line1_array[1000];
+    char line2_array[1000];
+    char* line1 = "";
+    char* line2 = entries[line2_counter];
+    while(line2 != NULL)
     {
-        dest = 0;
-        source = 0;
-        rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-        rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+        // printf("Lines %d-%d: %d\n", line1_counter, line2_counter, calc_difference(line1, line2, strlen(line1), strlen(line2)));
+        results[line2_counter] = calc_difference(line1, line2, strlen(line1), strlen(line2));
+        line1_counter++;
+        line2_counter++;
+        line1 = entries[line1_counter];
+        line2 = entries[line2_counter];
     }
 
+    gettimeofday(&t4, NULL);
+    MPI_Reduce(entries, entries, NUM_ENTRIES * LINE_LENGTH, MPI_CHAR, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
+	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+	printf("Time to read file: %f\n", elapsedTime);
+
+	elapsedTime = (t4.tv_sec - t3.tv_sec) * 1000.0; //sec to ms
+	elapsedTime += (t4.tv_usec - t3.tv_usec) / 1000.0; // us to ms
+	printf("Time to calculate difference: %f\n", elapsedTime);
+
+    printf("DATA, %d, %s, %f\n", myVersion, getenv("SLURM_CPUS_ON_NODE"), elapsedTime);
+
+
+    // if(rank == 0)
+    // {
+    //     dest = 1;
+    //     source = 1;
+    //     rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    //     rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+    // }
+    // else if (rank == 1)
+    // {
+    //     dest = 0;
+    //     source = 0;
+    //     rc = MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
+    //     rc = MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
+    // }
+
     MPI_Finalize();
+    return(0);
 }
 
 int calc_difference(char* line1, char* line2, int line1_length, int line2_length)
@@ -82,21 +132,6 @@ void read_file()
         strcpy(entries[i], line);
         i++;
     }
-
-    // int line1_counter = 0;
-    // int line2_counter = 1;
-    // char line1_array[1000];
-    // char line2_array[1000];
-    // char* line1 = "";
-    // char* line2 = fgets(line2_array, 1000, fp);
-    // while(line2 != NULL)
-    // {
-    //     printf("Lines %d-%d: %d\n", line1_counter, line2_counter, calc_difference(line1, line2, strlen(line1), strlen(line2)));
-    //     line1_counter++;
-    //     line2_counter++;
-    //     line1 = strcpy(line1_array, line2);
-    //     line2 = fgets(line2_array, 1000, fp);
-    // }
 
     fclose(fp);
 ]
