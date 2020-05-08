97b25d04cd86aa79a72f0245ceb8c6fae85d32f3
Blake Wewer <bewewer@eos.beocat.ksu.edu>
2020-05-08 09:30:34 -0500

    Began working with submitting to beocat

--- src/3way-mpi/mpi-batch.sh
+++ src/3way-mpi/mpi-batch.sh
@@ -2,4 +2,4 @@
 
 module load OpenMPI
 
-mpirun //usr/bin/time -o /homes/bewewer/proj4/CIS520P4/src/3way-mpi/time/time-10k/time-$RANDOM.txt /homes/bewewerproj4/CIS520P4/src/3way-mpi/output/output-10k/mpi-10k
+mpirun //usr/bin/time -o /homes/bewewer/proj4/CIS520P4/src/3way-mpi/time/time-$RANDOM.txt /homes/bewewerproj4/CIS520P4/src/3way-mpi/output/mpi
