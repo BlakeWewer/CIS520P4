1722c93514ba5ab698bba96f02feb80ddcb9e7e7
Dominic Tassio <dominic.tassio@gmail.com>
2020-05-11 22:07:40 -0500

    fix directory in openmp-batch.sh

--- src/3way-openmp/output/openmp-batch.sh
+++ src/3way-openmp/output/openmp-batch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-//usr/bin/time -o /homes/dmtassio/proj4/CIS520P4/src/3way-openmp/time/time-$RANDOM.txt /homes/dmtassio/CIS520P4/src/3way-openmp/openmp-prod
+//usr/bin/time -o /homes/dmtassio/proj4/CIS520P4/src/3way-openmp/time/time-$RANDOM.txt /homes/dmtassio/proj4/CIS520P4/src/3way-openmp/openmp-prod
