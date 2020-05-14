4eac2592d623ab785699200faf728ef17ed5ef99
Blake Wewer <bewewer@eos.beocat.ksu.edu>
2020-05-13 19:29:08 -0500

    I believe it may be mpi may be working now.

--- src/3way-mpi/output/mpi-batch.sh
+++ src/3way-mpi/output/mpi-batch.sh
@@ -2,4 +2,4 @@
 
 module load OpenMPI
 
-mpirun //usr/bin/time -o /homes/bewewer/proj4/CIS520P4/src/3way-mpi/time/time-$RANDOM.txt /homes/bewewerproj4/CIS520P4/src/3way-mpi/mpi-prod
+mpirun //usr/bin/time -o /homes/bewewer/proj4/CIS520P4/src/3way-mpi/time/time-$RANDOM.txt /homes/bewewerproj4/CIS520P4/src/3way-mpi/output/mpi-prod
